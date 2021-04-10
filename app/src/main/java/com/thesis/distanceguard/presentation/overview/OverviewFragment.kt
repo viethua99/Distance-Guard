b34904548242cadbd8cccdcd24d56bb5d217b435
Viet Hua <huaviet999@gmail.com>
2021-04-10 10:52:49 +0700

    add pie chart

--- app/src/main/java/com/thesis/distanceguard/presentation/overview/OverviewFragment.kt
+++ app/src/main/java/com/thesis/distanceguard/presentation/overview/OverviewFragment.kt
@@ -1,8 +1,13 @@
 package com.thesis.distanceguard.presentation.overview
 
+import android.graphics.Color
 import android.view.View
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
 import com.thesis.distanceguard.R
 import com.thesis.distanceguard.presentation.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_overview.*
 
 class OverviewFragment : BaseFragment() {
     override fun getResLayoutId(): Int {
@@ -10,6 +15,47 @@ class OverviewFragment : BaseFragment() {
     }
 
     override fun onMyViewCreated(view: View) {
+       showPieChart()
+    }
 
+    private fun showPieChart() {
+        val pieEntries: ArrayList<PieEntry> = ArrayList()
+        val label = "type"
+
+        //initializing data
+        val typeAmountMap: MutableMap<String, Int> = HashMap()
+        typeAmountMap["Toys"] = 200
+        typeAmountMap["Snacks"] = 230
+        typeAmountMap["Clothes"] = 100
+        typeAmountMap["Stationary"] = 500
+        typeAmountMap["Phone"] = 50
+
+        //initializing colors for the entries
+        val colors: ArrayList<Int> = ArrayList()
+        colors.add(Color.parseColor("#304567"))
+        colors.add(Color.parseColor("#309967"))
+        colors.add(Color.parseColor("#476567"))
+        colors.add(Color.parseColor("#890567"))
+        colors.add(Color.parseColor("#a35567"))
+        colors.add(Color.parseColor("#ff5f67"))
+        colors.add(Color.parseColor("#3ca567"))
+
+        //input data and fit data into pie chart entry
+        for (type in typeAmountMap.keys) {
+            pieEntries.add(PieEntry(typeAmountMap[type]!!.toFloat(), type))
+        }
+
+        //collecting the entries with label name
+        val pieDataSet = PieDataSet(pieEntries, label)
+        //setting text size of the value
+        pieDataSet.valueTextSize = 12f
+        //providing color list for coloring different entries
+        pieDataSet.colors = colors
+        //grouping the data set from entry to chart
+        val pieData = PieData(pieDataSet)
+        //showing the value of the entries, default true if not set
+        pieData.setDrawValues(true)
+        pie_chart.data = pieData
+        pie_chart.invalidate()
     }
 }
