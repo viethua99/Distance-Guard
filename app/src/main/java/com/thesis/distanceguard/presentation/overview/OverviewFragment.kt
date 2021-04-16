23b23aba3460fffe7cf64efad92ebe16bab72abb
Viet Hua <huaviet999@gmail.com>
2021-04-16 15:21:41 +0700

    add fab

--- app/src/main/java/com/thesis/distanceguard/presentation/overview/OverviewFragment.kt
+++ app/src/main/java/com/thesis/distanceguard/presentation/overview/OverviewFragment.kt
@@ -3,6 +3,9 @@ package com.thesis.distanceguard.presentation.overview
 import android.view.View
 import com.thesis.distanceguard.R
 import com.thesis.distanceguard.presentation.base.BaseFragment
+import com.thesis.distanceguard.presentation.main.MainActivity
+import com.thesis.distanceguard.presentation.map.MapFragment
+import kotlinx.android.synthetic.main.fragment_overview.*
 
 class OverviewFragment : BaseFragment() {
     override fun getResLayoutId(): Int {
@@ -10,6 +13,9 @@ class OverviewFragment : BaseFragment() {
     }
 
     override fun onMyViewCreated(view: View) {
-
+        fab_map.setOnClickListener {
+            val mainActivity = activity as MainActivity
+            mainActivity.replaceFragment(MapFragment(),"Map Fragment",R.id.container_main)
+        }
     }
 }
