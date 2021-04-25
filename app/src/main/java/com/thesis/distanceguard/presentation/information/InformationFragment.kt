67981707261c365108873e340ba2931df361775a
Viet Hua <huaviet999@gmail.com>
2021-04-25 19:56:53 +0700

    change status bar color

--- app/src/main/java/com/thesis/distanceguard/presentation/information/InformationFragment.kt
+++ app/src/main/java/com/thesis/distanceguard/presentation/information/InformationFragment.kt
@@ -1,6 +1,7 @@
 package com.thesis.distanceguard.presentation.information
 
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.thesis.distanceguard.R
 import com.thesis.distanceguard.presentation.base.BaseFragment
 import com.thesis.distanceguard.presentation.main.activity.MainActivity
@@ -20,11 +21,14 @@ class InformationFragment : BaseFragment(){
     override fun onMyViewCreated(view: View) {
         val mainActivity = activity as MainActivity
         mainActivity.appBarLayout.visibility = View.GONE
+        mainActivity.window.statusBarColor = ContextCompat.getColor(context!!,R.color.header_red)
     }
 
     override fun onStop() {
         super.onStop()
         val mainActivity = activity as MainActivity
         mainActivity.appBarLayout.visibility = View.VISIBLE
+        mainActivity.window.statusBarColor = ContextCompat.getColor(context!!,R.color.gray_background_color)
+
     }
 }
