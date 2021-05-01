20b11ae6aaadb2d5a9e09641843a55937103e799
ĐVũ <nguyenducvu26051999@gmail.com>
2021-05-01 14:39:47 +0700

    add detail country & change contructor detailFragment

--- app/src/main/java/com/thesis/distanceguard/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/thesis/distanceguard/presentation/detail/DetailFragment.kt
@@ -4,14 +4,28 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.thesis.distanceguard.R
+import com.thesis.distanceguard.api.model.CountryResponse
+import com.thesis.distanceguard.api.model.HistoricalWorldwideResponse
 import com.thesis.distanceguard.presentation.base.BaseFragment
 import com.thesis.distanceguard.presentation.main.activity.MainActivity
+import com.thesis.distanceguard.util.AppUtil
+import dagger.android.support.AndroidSupportInjection
+import kotlinx.android.synthetic.main.fragment_detail.*
 import timber.log.Timber
 
-class DetailFragment : BaseFragment() {
+class DetailFragment() : BaseFragment() {
+    constructor(item: CountryResponse) : this() {
+        itemCountry = item
+    }
+
     companion object {
         const val TAG = "DetailFragment"
+        private lateinit var detailViewModel: DetailViewModel
+        lateinit var itemCountry: CountryResponse
+
     }
 
 
@@ -21,13 +35,16 @@ class DetailFragment : BaseFragment() {
 
     override fun onMyViewCreated(view: View) {
         setHasOptionsMenu(true)
+
+        updateMainInformation()
+        updateOtherInformation()
     }
 
 
     override fun onResume() {
         super.onResume()
         Timber.d("onResume")
-        setupToolbarTitle("France")
+        setupToolbarTitle(itemCountry.country)
         (activity as MainActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
@@ -38,6 +55,31 @@ class DetailFragment : BaseFragment() {
         (activity as MainActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
     }
 
+    private fun updateMainInformation() {
+        tv_total_cases_count.text = AppUtil.toNumberWithCommas(itemCountry.cases.toLong())
+        tv_recover_count.text = AppUtil.toNumberWithCommas(itemCountry.recovered.toLong())
+        tv_active_count.text = AppUtil.toNumberWithCommas(itemCountry.active.toLong())
+        tv_deaths_count.text = AppUtil.toNumberWithCommas(itemCountry.deaths.toLong())
+    }
+
+    private fun updateOtherInformation() {
+        tv_test_count.text = AppUtil.toNumberWithCommas(itemCountry.tests.toLong())
+        tv_population_count.text = AppUtil.toNumberWithCommas(itemCountry.population.toLong())
+        tv_one_case_per_people_count.text =
+            AppUtil.toNumberWithCommas(itemCountry.oneCasePerPeople.toLong())
+        tv_one_test_per_people_count.text =
+            AppUtil.toNumberWithCommas(itemCountry.oneTestPerPeople.toLong())
+        tv_one_death_per_people_count.text =
+            AppUtil.toNumberWithCommas(itemCountry.oneDeathPerPeople.toLong())
+        tv_case_per_one_million_count.text =
+            AppUtil.toNumberWithCommas(itemCountry.casesPerOneMillion.toLong())
+        tv_death_per_one_million_count.text =
+            AppUtil.toNumberWithCommas(itemCountry.deathsPerOneMillion.toLong())
+        tv_test_per_one_million_count.text =
+            AppUtil.toNumberWithCommas(itemCountry.testsPerOneMillion.toLong())
+        tv_active_per_one_million_count.text =
+            AppUtil.toNumberWithCommas(itemCountry.activePerOneMillion.toLong())
+    }
 
 
 }
